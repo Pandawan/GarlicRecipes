bb0c0fd5ebeb769c852b0091048bdc5917c0ffe5
migueltenant <migueltenant@gmail.com>
2018-01-09 20:08:36 -0500

    Setup everything, ready to code!

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,92 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+const pump = require('pump');
+const htmlclean = require('gulp-htmlclean');
+const eslint = require('gulp-eslint');
 const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const cssnano = require('cssnano');
+const ghpages = require('gh-pages');
 
-gulp.task('scripts', function() {
-	return gulp.src('src/js/**/*.js')
-		.pipe(concat('scripts.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(rename('scripts.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+
+// Clean HTML
+gulp.task('html', function(cb) {
+	pump([
+		gulp.src('src/**/*.html'),
+		htmlclean(),
+		gulp.dest('dist/')
+	], cb);
 });
 
-gulp.task('watch', ['scripts'], function() {
-  gulp.watch('src/js/**/*.js', ['scripts'])
+// Run task js, only if verify is successful
+gulp.task('js', ['verify'], function(cb) {
+	pump([
+		gulp.src(['src/js/**/*.js', '!src/lib/**/*']),
+		concat('script.js'),
+		gulp.dest('dist/js'),
+		minify({
+			ext: {
+				min: '.min.js'
+			},
+			noSource: true
+		}),
+		gulp.dest('dist/js')
+	], cb);
+});
+
+// Lints the js files
+gulp.task('verify', function(cb) {
+	pump([
+		gulp.src(['src/js/**/*.js', '!src/lib/**/*']),
+		eslint(),
+		eslint.format(),
+		eslint.failAfterError()
+	], cb);
+});
+
+// Copies any file in lib
+gulp.task('lib', function(cb) {
+	pump([
+		gulp.src(['src/lib/**/*']),
+		gulp.dest('dist/lib')
+	], cb);
+});
+
+// Do a bunch of stuff to CSS files
+gulp.task('less', function(cb) {
+	pump([
+		gulp.src('src/less/**/*.less'),
+		less(),
+		postcss([
+			autoprefixer({
+				browsers: ['last 2 versions', '> 2%']
+			}),
+			mqpacker
+		]),
+		rename('style.css'),
+		gulp.dest('dist/css'),
+		postcss([
+			cssnano
+		]),
+		rename('style.min.css'),
+		gulp.dest('dist/css')
+	], cb);
+});
+
+gulp.task('deploy', ['build'], function(cb) {
+  ghpages.publish('dist', cb);
+});
+
+gulp.task('build', ['js', 'less', 'html', 'lib']);
+
+gulp.task('watch', ['js', 'less', 'html', 'lib'], function() {
+	gulp.watch('src/js/**/*.js', ['js'])
+	gulp.watch('src/less/**/*.less', ['less'])
+	gulp.watch('src/html/**/*.html', ['html'])
+	gulp.watch('src/lib/**/*', ['lib'])
+
 });
